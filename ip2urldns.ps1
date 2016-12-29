7f6fc71411affa8abf9bd904a8e2b732ee13eddd
tmcmil <tyler.r.perdue@gmail.com>
2016-12-29 18:49:44 -0500

    Add files via upload

---
+++ ip2urldns.ps1
@@ -0,0 +1,46 @@
+﻿###################################################################################################################
+#                                                   IP2URL.resolve.ps1                                            #
+#                                   Resolve DNS  and landing page of an ip spreadsheet                            #
+###################################################################################################################
+#Set Location of CSV hostlist with column headers "IP" containing all ips and an empty "URL" and "Hostname" column#
+###################################################################################################################
+$filepath = "Z:\HACKING\Google Drive\Google Drive\assessment\scripts\powershell\test.csv" 
+
+##############################
+#Import File in $ips variable#
+##############################
+$ips = Import-CSV $filepath
+
+############################################################################################################
+#for loop to interate through the ips from the csv IP column assigning each to "$ie.Navigate($ips[$i].IP)".#
+#Then browse to each IP with a COM call to IE and record the url of the landing page to "$ips[$i].URL".    #
+############################################################################################################
+ for ($i = 0; $i -lt $ips.Count; $i++)
+ {
+ $ie = New-Object -ComObject "InternetExplorer.Application"
+    $ie.Visible = $false
+    $ie.Silent = $true
+    $ie.Navigate($ips[$i].IP)
+    Do{Start-Sleep 1}While($ie.Busy -eq $true)
+    $ips[$i].URL = $ie.Document.url
+    
+#################################################################################
+#DNS resolution during the for loop for each IP. Writes to $ips[$i].Hostname    #
+#################################################################################
+  $result = $null
+  $currentEAP = $ErrorActionPreference
+  $ErrorActionPreference = "silentlycontinue"
+  $result = [System.Net.Dns]::GetHostByAddress($ips[$i].IP)
+  $ErrorActionPreference = $currentEAP
+  if ($result)
+    {
+     $ips[$i].HostName = [string]$result.HostName
+    }
+###############################################################
+#Record values and export to csv file columns URL and Hostname#
+###############################################################
+ Write-host $ips[$i].URL","$ips[$i].Hostname
+ $ips | Export-Csv "$filepath.out.csv"
+ Get-Process iexplore | Stop-Process
+ }
+ echo "Export Complete"
